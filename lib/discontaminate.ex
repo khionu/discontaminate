f5779aa1d52a73779fdc2d6d6a302981180d12b6
Khionu Sybiern <dev@khionu.net>
2020-05-04 16:26:48 -0500

    Add initial elixir project

---
+++ lib/discontaminate.ex
@@ -0,0 +1,18 @@
+defmodule Discontaminate do
+  @moduledoc """
+  Documentation for `Discontaminate`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Discontaminate.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/discontaminate_test.exs
@@ -0,0 +1,8 @@
+defmodule DiscontaminateTest do
+  use ExUnit.Case
+  doctest Discontaminate
+
+  test "greets the world" do
+    assert Discontaminate.hello() == :world
+  end
+end
